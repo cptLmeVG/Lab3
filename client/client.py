64b78adfcf81dc9804a841259672b4664eef7015
cptLmeVG <58371467+cptLmeVG@users.noreply.github.com>
2019-11-30 21:49:22 +0300

    Add files via upload

---
+++ client/client.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import psutil
+import json
+import requests
+import configparser
+from time import sleep
+
+# returns dict of total, used and available memory
+def get_memory_info():
+    memory = psutil.virtual_memory()
+    delimeter = 1024.0 ** 3
+    memory_info = {
+            'total memory' : str(round(memory.total / delimeter, 1))+"Gb",
+            'memory used' : str(round(memory.used / delimeter, 1))+"Gb",
+            'available memory' : str(round(memory.available / delimeter, 1))+"Gb"
+            }
+    return memory_info
+
+
+def get_processor_info():
+    processor = {
+            'processor_load' : (psutil.cpu_percent())
+            }
+    print(processor)
+    return processor
+
+
+def get_disk_freespace_info():
+    disk = psutil.disk_usage("/")
+    free = {'disk free space' : disk.free / (1024.0 ** 3)}
+    return free
+
+def get_procs_count():
+    quantity = 0
+    for _ in psutil.process_iter():
+        quantity += 1
+    return { 'processes' : quantity }
+
+
+def get_users_list():
+    users = psutil.users()
+    users_string = str()
+    for entry in users:
+        if " " + entry[0] + " " not in users_string:
+            # users_list.append(entry[0])
+            users_string += f"{entry[0]} "
+    return { 'users' : users_string}
+
+
+#users = get_users_list()
+#with open("./users.txt", "a+") as f:
+#    f.write(json.dumps(users))
+def get_all_info():
+    memory_info = get_memory_info()
+    cpu_info = get_processor_info()
+    disk_space = get_disk_freespace_info()
+    procs_count = get_procs_count()
+    user_list = get_users_list()
+
+    data = {}
+    data.update(memory_info)
+    data.update(cpu_info)
+    data.update(disk_space)
+    data.update(procs_count)
+    data.update(user_list)
+    return data
+
+def config_read():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    server_ip = config['default']['ip']
+    server_port = config['default']['port']
+    timeout = config['default']['timeout']
+    return server_ip, server_port, int(timeout)
+
+# data of type dict
+def send_data(host='localhost', port=5000, data={}):
+    print(f"Sending data to {host}:{port}")
+    req = requests.post(f"http://{host}:{port}/api/data", json=data)
+    if req.status_code != 200:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    raddr, rport, timeout = config_read()
+    while True:
+        raddr, rport, timeout = config_read()
+        info_dict = get_all_info()
+        send_data(raddr, rport, info_dict)
+        sleep(timeout)
+
---
+++ server/app.py
@@ -0,0 +1,77 @@
+# /api/data doesn't insert or update any data... select works ok..
+from flask import Flask, request, render_template, render_template_string, g, current_app
+import json
+import socket
+import sqlite3
+
+app = Flask(__name__, static_url_path='/static')
+
+HOST = '127.0.0.1'
+PORT = 1337
+DATABASE_PATH = "/root/Загрузки/python-labs-master/3rd-lab/server/info.db"
+#conn = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
+#cursor = conn.cursor()
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE_PATH)
+        db.row_factory = sqlite3.Row
+    return db
+   # db = getattr(g, '_database', None)
+   # if db is None:
+   #     db = g._database = sqlite3.connect(DATABASE_PATH)
+
+@app.route("/")
+def index():
+    #users_json = json.loads(raw_users)
+    conn = sqlite3.connect(DATABASE_PATH)
+    cur = conn.cursor()
+    cur.execute("SELECT server, data FROM data_table")
+    rows = cur.fetchall()
+    new_list = {}
+    if not rows:
+        return render_template('index.html', ip=None, data={})
+    for row in rows:
+#        temp_json = json.loads(var['data'])
+        #print("=>: ", row['data'])
+        #print("json: ", type(row['data']))
+        print(row)
+        #ip = row['server']
+        ip = row[0]
+        try:
+            #json_l = json.loads(row['data'])
+            json_l = json.loads(row[1])
+        except:
+            json_l = {'data' : 'Null'}
+        new_list[ip] = json_l
+    conn.close()
+    return render_template('index.html', ip=ip, data=new_list)
+
+
+@app.route("/api/data", methods=["POST", "GET"])
+def data_receive():
+    # insert with sqlite3.connect(DBPATH) as conn:
+    conn = sqlite3.connect(DATABASE_PATH)
+    curs = conn.cursor()
+    #curs = get_db().cursor()
+    print(f"req.getjson: {request.get_json()}")
+    post = json.dumps(request.get_json())
+    ip = request.remote_addr
+    print(f"[!] POST: {post} <=> HOST: {(ip)}")
+    print(curs.description)
+    curs.execute("""INSERT INTO data_table (`server`, `data`) VALUES (?, ?)""", (ip, post))
+    conn.commit()
+    conn.close()
+    return "Heh"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
